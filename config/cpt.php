51968edca925f8d6873ccd82190624086aef83de
David Egan <david@carawebs.com>
2017-03-23 15:46:11 +0000

    Library branch - set up CPTS and custom taxonomies from a Composer library rather than a plugin. This might be useful if bundling CPT registration within a theme, bedrock, or even another plugin.

---
+++ config/cpt.php
@@ -0,0 +1,37 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+$cpts = [
+    [
+        'slug'          => 'projectoo',
+        'singular_name' => 'projectoo',
+        'plural_name'   => 'projectoos',
+        'args'          => [
+            'menu_icon'   => 'dashicons-analytics',
+            'has_archive' => true,
+            'taxonomies'  => [ 'post_tag' ],
+        ]
+    ],
+    [
+        'slug'          => 'landing-page',
+        'singular_name' => 'Landing Page',
+        'plural_name'   => 'Landing Pages',
+        'args'          => ['menu_icon' => 'dashicons-cart']
+
+    ],
+    [
+        'slug'          => 'service',
+        'singular_name' => 'service',
+        'plural_name'   => 'services',
+        'args'          => ['menu_icon' => 'dashicons-hammer']
+
+    ],
+    [
+        'slug'          => 'person',
+        'singular_name' => 'person',
+        'plural_name'   => 'people',
+        'args'          => ['menu_icon' => 'dashicons-groups']
+
+    ]
+];
+
+return $cpts;
---
+++ config/tax.php
@@ -0,0 +1,24 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+$taxonomies = [
+    [
+        'slug'          => 'project-category',
+        'singular_name' => 'project category',
+        'plural_name'   => 'project categories',
+        'related_CPTs'  => [
+            'project'
+        ],
+        'override_args' => [
+            'hierarchical' => false
+        ]
+    ],
+    [
+        'slug'          => 'service-category',
+        'singular_name' => 'service category',
+        'plural_name'   => 'service categories',
+        'related_CPTs'  => [
+            'service'
+        ],
+    ],
+];
+return $taxonomies;
