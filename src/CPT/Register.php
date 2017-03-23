888eb8f74eebf64395e2a8c3d4c18b711b569b6f
David Egan <david@carawebs.com>
2017-03-23 18:12:26 +0000

    Add CPT specific 'View Posts' text. Tidy up.

--- src/CPT/Register.php
+++ src/CPT/Register.php
@@ -42,12 +42,6 @@ class Register
         return $this;
     }
 
-    // public function setName($slug, $singular_name, $plural_name)
-    // {
-    //     $this->slug = $slug;
-    //     $this->singular_name = $singular_name;
-    //     $this->plural = $plural_name;
-    // }
     public function setOverrides($overrides)
     {
         $this->overrides = $overrides;
@@ -61,7 +55,6 @@ class Register
     */
     public function setLabels ()
     {
-
         $this->labels = [
             'name'                => _x( ucfirst( $this->plural ), 'Post Type General Name', 'CARAWEBS' ),
             'singular_name'       => _x( ucfirst( $this->singular_name ), 'Post Type Singular Name', 'CARAWEBS' ),
@@ -75,10 +68,10 @@ class Register
             'edit_item'           => __( 'Edit ' . ucfirst( $this->singular_name ), 'CARAWEBS' ),
             'update_item'         => __( 'Update ' . ucfirst( $this->singular_name ), 'CARAWEBS' ),
             'view_item'           => __( 'View ' . ucfirst( $this->singular_name ), 'CARAWEBS' ),
+            'view_items'          => __( 'View ' . ucfirst( $this->plural ), 'CARAWEBS' ),
             'search_items'        => __( 'Search ' . ucfirst( $this->plural ), 'CARAWEBS' ),
             'not_found'           => __( 'Not found', 'CARAWEBS' ),
             'not_found_in_trash'  => __( 'Not found in Trash', 'CARAWEBS' ),
-
         ];
     }
 
@@ -92,7 +85,6 @@ class Register
     */
     public function init()
     {
-
         $defaults = [
             'label'               => __( ucfirst( $this->singular_name ), 'CARAWEBS' ),
             'description'         => __( ucfirst( $this->singular_name ) . ' posts', 'CARAWEBS' ),
@@ -114,9 +106,7 @@ class Register
         ];
 
         $args = is_array($this->overrides) ? array_merge($defaults, $this->overrides) : $default;
-
         register_post_type($this->slug, $args);
-
     }
 
     /**
@@ -130,12 +120,10 @@ class Register
     */
     public function messages($messages)
     {
-
         global $post;
         $permalink = get_permalink( $post );
         $capname = ucfirst( $this->singular_name );
         $messages[$this->slug] = [
-
             0 => '', // Unused. Messages start at index 1.
             1 => sprintf( __('%s updated. <a target="_blank" href="%s">View %s</a>', 'CARAWEBS'),
                 $capname,
